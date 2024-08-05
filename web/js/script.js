44ec2267d72b56e2e18629bd3d87e89588a340a2
AJEESH RAJAN <ajeesh.pentacodesllp@gmail.com>
2024-08-05 10:55:18 +0530

    lat commit

--- web/js/script.js
+++ web/js/script.js
@@ -135,8 +135,6 @@ $(document).ready(function () {
 //   observer.observe(video);
 // });
 
-
-
 document.addEventListener("DOMContentLoaded", function() {
   const video = document.getElementById('videoBanner');
   if (video) {
@@ -150,6 +148,18 @@ document.addEventListener("DOMContentLoaded", function() {
       });
       observer.observe(video);
   }
+  const video2 = document.getElementById('videoCta');
+  if (video2) {
+      const observer = new IntersectionObserver(entries => {
+          entries.forEach(entry => {
+              if (entry.isIntersecting) {
+                video2.load();
+                  observer.disconnect();
+              }
+          });
+      });
+      observer.observe(video2);
+  }
 });
 
 
@@ -158,7 +168,6 @@ document.addEventListener('DOMContentLoaded', function() {
   specialityDivs.forEach((div, index) => {
       if (index < specialityDivs.length - 1) {
           const iElement = document.createElement('i');
-          // iElement.innerHTML = '<svg width="110" height="34" viewBox="0 0 110 34" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="55" cy="17" r="16.75" stroke="url(#paint0_linear_532_3351)" stroke-width="0.5"/> <circle cx="55" cy="17" r="16.75" stroke="url(#paint1_linear_532_3351)" stroke-width="0.5"/> <line y1="16.75" x2="110" y2="16.75" stroke="url(#paint2_linear_532_3351)" stroke-width="0.5"/> <line y1="16.75" x2="110" y2="16.75" stroke="url(#paint3_linear_532_3351)" stroke-width="0.5"/> <circle cx="55" cy="17" r="1.5" fill="white" stroke="url(#paint4_linear_532_3351)"/> <defs> <linearGradient id="paint0_linear_532_3351" x1="38" y1="17" x2="72" y2="17" gradientUnits="userSpaceOnUse"> <stop stop-color="#3412E0"/> <stop offset="0.479138" stop-color="#7928E5"/> <stop offset="1" stop-color="#07B4CC"/> </linearGradient> <linearGradient id="paint1_linear_532_3351" x1="38" y1="17" x2="72" y2="17" gradientUnits="userSpaceOnUse"> <stop stop-color="#6F7E97"/> <stop offset="1" stop-color="#242931"/> </linearGradient> <linearGradient id="paint2_linear_532_3351" x1="0" y1="17.5" x2="110" y2="17.5" gradientUnits="userSpaceOnUse"> <stop stop-color="#3412E0"/> <stop offset="0.479138" stop-color="#7928E5"/> <stop offset="1" stop-color="#07B4CC"/> </linearGradient> <linearGradient id="paint3_linear_532_3351" x1="0" y1="17.5" x2="110" y2="17.5" gradientUnits="userSpaceOnUse"> <stop stop-color="#6F7E97"/> <stop offset="1" stop-color="#242931"/> </linearGradient> <linearGradient id="paint4_linear_532_3351" x1="53" y1="17" x2="57" y2="17" gradientUnits="userSpaceOnUse"> <stop stop-color="#6F7E97"/> <stop offset="1" stop-color="#242931"/> </linearGradient> </defs> </svg>'
           iElement.innerHTML = '<img src= "web/images/icons/step.webp" alt="icon"  width="110" height="34">'
           div.parentNode.insertBefore(iElement, div.nextSibling);
       }
@@ -179,3 +188,13 @@ $(document).ready(function () {
 // ======================Lazy Loading ===============================
 
 document.addEventListener("DOMContentLoaded", function() {var lazyImages = document.querySelectorAll("img[data-src][loading='lazy']");var lazyImageObserver = new IntersectionObserver(function(entries, observer) {entries.forEach(function(entry) {if (entry.isIntersecting) {var lazyImage = entry.target;lazyImage.src = lazyImage.dataset.src;lazyImage.alt = lazyImage.dataset.alt;lazyImage.removeAttribute("data-src");lazyImage.removeAttribute("loading");lazyImageObserver.unobserve(lazyImage);}});});lazyImages.forEach(function(lazyImage) {lazyImageObserver.observe(lazyImage);});});
+
+// ======================Scroll Top ===============================
+
+let mybutton = document.getElementById("scrollBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+mybutton.addEventListener('click', function() {
+  window.scrollTo({top: 0, behavior: 'smooth'});
+});
